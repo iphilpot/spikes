1d2b2522e33d7f825c748f87f2dd374a1bd9fbb6
Ian Philpot <ian.philpot@microsoft.com>
2019-11-25 10:18:48 -0500

    Adds weak container flow to main.py

--- aci_tasks/aci_tasks/main.py
+++ aci_tasks/aci_tasks/main.py
@@ -1,17 +1,32 @@
 #!/usr/bin/env python
+import time
 from config import Config, TaskType
 from logger import Logger
 from cloud import ContainerGroupInstance, ResourceGroup, ContainerInstance
 
 if __name__ == "__main__":
-    config = Config(TaskType.STRONG)
+    # Create Strong task container and exec command
+    strong_config = Config(TaskType.STRONG)
     logger = Logger()
 
-    rg = ResourceGroup(logger, config)
+    rg = ResourceGroup(logger, strong_config)
     rg.create()
 
-    cg = ContainerGroupInstance(logger, config)
-    cg.create()
+    strong_container_group = ContainerGroupInstance(logger, strong_config)
+    strong_container_group.create()
 
-    c = ContainerInstance(logger, config)
-    c.execute_command('/strong_task/dog.py')
+    strong_container = ContainerInstance(logger, strong_config)
+    strong_container.execute_command('/strong_task/dog.py')
+
+    # Create week task container and start/stop to exec command
+    weak_config = Config(TaskType.WEAK)
+
+    weak_container_group = ContainerGroupInstance(logger, weak_config)
+    weak_container_group.create()
+
+    weak_container_group.stop()
+
+    time.sleep(60)
+
+    weak_container_group.start()
+    weak_container_group.stop()
