639e1c42aa22bdf9ce1a04c5244db6b7b4d08c47
Ian Philpot <ian.philpot@microsoft.com>
2020-05-25 11:50:08 -0400

    Orchestator boiler plate

---
+++ AggMachine/Aggregator.Functions/orchestrations/Orchestrator.cs
@@ -0,0 +1,41 @@
+namespace Aggregator.Functions.Orchestrations
+{
+    public static class Orchestrator
+    {
+        [FunctionName("Orchestrator")]
+        public static async Task RunOrchestrator(
+            [OrchestrationTrigger] IDurableOrchestrationContext context, ILogger log)
+        {
+
+            // This will ALWAYS read the persisted state from the entity key!
+            // Destroy contents of entity's Azure Stor when modifying entity
+            try
+            {
+                InventoryEvent data = context.GetInput<InventoryEvent>();
+
+                if (data == null)
+                {
+                    throw new ArgumentException("data received to orchestrator is null");
+                }
+
+                var entityId = new EntityId("StoreEntity", data.StoreId);
+                context.SignalEntity(entityId, "Aggregate", data);
+
+                // Two-way call to the entity which returns a value - awaits the response
+                Item sampleItem  = await context.CallEntityAsync<Item>(entityId, "GetItem", data.Id).ConfigureAwait(false);
+
+                if (sampleItem != null)
+                {
+                    log.LogInformation(sampleItem.Id);
+                }
+                              
+            }
+            catch (Exception e)
+            {
+                log.LogError(e.Message);
+                
+            }          
+
+        }
+    }
+}
